9a5cb5e0890722afb28a21b825f5147a872d96ca
xorrr <derxor@gmail.com>
2014-01-11 19:22:55 +0100

    add example on how to use the jsoup with url

--- src/test/java/org/xorrr/TestCase.java
+++ src/test/java/org/xorrr/TestCase.java
@@ -2,20 +2,28 @@ package org.xorrr;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlDivision;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
-
 public class TestCase {
 
+    @Ignore
     @Test
     public void testPage() throws Exception {
         final WebClient webClient = new WebClient();
 
-        final HtmlPage page = webClient.getPage("http://localhost:8080/financegrabber");
+        final HtmlPage page = webClient
+                .getPage("http://localhost:8080/financegrabber");
 
         webClient.waitForBackgroundJavaScript(5000);
 
@@ -25,5 +33,17 @@ public class TestCase {
         assertEquals("button", div.getAttribute("role"));
 
         webClient.closeAllWindows();
+    }
+
+    @Test
+    public void testJsoup() throws Exception {
+
+        String test = "http://www.boerse-frankfurt.de/de/suche/"
+                + "ergebnis?order_by=wm_vbfw.name&name_isin_wkn=LU0048578792";
+
+        Document doc = Jsoup.connect(test).userAgent("Mozilla").get();
+
+        assertEquals(doc.getElementsByAttributeValue("class", "info").get(0)
+                .child(0).text(), "FF - European Growth");
     }
 }
