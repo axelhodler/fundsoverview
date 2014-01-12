156bacb47305ed22d729ef7333da28f01ccaa2b3
xorrr <derxor@gmail.com>
2014-01-12 22:41:59 +0100

    organize imports in vaadin ui

--- src/main/java/org/xorrr/financegrabber/MyVaadinUI.java
+++ src/main/java/org/xorrr/financegrabber/MyVaadinUI.java
@@ -7,8 +7,6 @@ import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
