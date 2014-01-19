c5ab583aa5eaa5f49a300bdb3af964aad05e5525
xorrr <derxor@gmail.com>
2014-01-19 01:59:22 +0100

    add unit test for the view

---
+++ src/test/java/org/xorrr/financegrabber/view/TestFinanceGrabberViewImpl.java
@@ -0,0 +1,30 @@
+package org.xorrr.financegrabber.view;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.xorrr.financegrabber.presenter.FinanceGrabberViewHandler;
+
+public class TestFinanceGrabberViewImpl {
+
+    private FinanceGrabberView view;
+    private FinanceGrabberViewHandler handler;
+
+    @Before
+    public void setUp() {
+        view = new FinanceGrabberViewImpl();
+        handler = mock(FinanceGrabberViewHandler.class);
+        view.addHandler(handler);
+        view.init();
+    }
+
+    @Test
+    public void testIfAddFundMethodIsTriggeredWhenButtonIsClicked() {
+        view.getAddFundBtn().click();
+
+        verify(handler, times(1)).addFund("");
+    }
+}
