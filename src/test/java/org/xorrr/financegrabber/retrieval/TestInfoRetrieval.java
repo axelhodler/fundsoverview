40bc7a42546d6bbcdd42299751a61b3667bdaab3
xorrr <derxor@gmail.com>
2014-01-25 21:28:40 +0100

    add example test on how to deal with Elements in example.html

--- src/test/java/org/xorrr/financegrabber/retrieval/TestInfoRetrieval.java
+++ src/test/java/org/xorrr/financegrabber/retrieval/TestInfoRetrieval.java
@@ -1,11 +1,15 @@
 package org.xorrr.financegrabber.retrieval;
 
+import static org.junit.Assert.*;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.junit.Test;
 
 public class TestInfoRetrieval {
 
@@ -16,5 +20,13 @@ public class TestInfoRetrieval {
         Document doc = Jsoup.parse(file, "UTF-8");
 
         return doc;
+    }
+
+    @Test
+    public void testGettingTheValue() throws IOException {
+        Document doc = getDocumentFromHtmlFile();
+        
+        Element e = doc.getElementsByClass("priceValue").get(0);
+        assertEquals("27,74$", e.html().replace("&nbsp;", ""));
     }
 }
