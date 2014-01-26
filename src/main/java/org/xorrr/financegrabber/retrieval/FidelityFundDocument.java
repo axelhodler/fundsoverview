acf73ea7f1ea1a095e2840a3a3be92fd9c2c9a49
xorrr <derxor@gmail.com>
2014-01-26 23:38:54 +0100

    multiple refactorings

--- src/main/java/org/xorrr/financegrabber/retrieval/FidelityFundDocument.java
+++ src/main/java/org/xorrr/financegrabber/retrieval/FidelityFundDocument.java
@@ -8,21 +8,27 @@ import org.jsoup.nodes.Document;
 public class FidelityFundDocument {
 
     private Document doc;
+    private String urlBase = "https://fww.biz/fidelity/direkt/fondsportraet/?&PARENT="
+            + "https%3A//www.fidelity.de/de/fonds/fonds-detailansicht.page%3FISIN%3D";
+    private String urlToFormat = "%s&ISIN=%s";
 
     public FidelityFundDocument(String isin) throws IOException, InvalidIsinException {
-        String urlBase = "https://fww.biz/fidelity/direkt/fondsportraet/?&PARENT="
-                + "https%3A//www.fidelity.de/de/fonds/fonds-detailansicht.page%3FISIN%3D";
-        String urlToFormat = "%s&ISIN=%s";
-        String formattedUrlPart = String.format(urlToFormat, isin, isin);
-
-        String url = urlBase + formattedUrlPart;
-        this.doc = Jsoup.connect(url).userAgent("Mozilla").get();
-        if (this.doc.body().html().equals("Invalid value for parameter ISIN."))
-            throw new InvalidIsinException("The provided ISIN was invalid");
-
+        String url = urlBase + String.format(urlToFormat, isin, isin);
+        this.doc = getDocumentWithJsoup(url);
+        checkIfIsinIsInvalid();
     }
 
     public Document getDocument() {
         return this.doc;
     }
+
+    private void checkIfIsinIsInvalid() throws InvalidIsinException {
+        if (this.doc.body().html().equals("Invalid value for parameter ISIN."))
+            throw new InvalidIsinException("The provided ISIN was invalid");
+    }
+
+    private Document getDocumentWithJsoup(String url) throws IOException {
+        return Jsoup.connect(url).userAgent("Mozilla").get();
+    }
+
 }
--- src/test/java/org/xorrr/financegrabber/retrieval/TestFundValuesExtractor.java
+++ src/test/java/org/xorrr/financegrabber/retrieval/TestFundValuesExtractor.java
@@ -4,19 +4,26 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestFundValuesExtractor {
 
-    @Test
-    public void testGettingThePrice() throws IOException {
-        FundValuesExtractor ret = new FundValuesExtractor();
-        assertEquals("27,74$", ret.extractPrice());
+    private FundValuesExtractor extractor;
+
+    @Before
+    public void setUp() throws IOException {
+        this.extractor = new FundValuesExtractor();
     }
 
     @Test
-    public void testGettingTheName() throws IOException {
-        FundValuesExtractor ret = new FundValuesExtractor();
-        assertEquals("Fidelity Funds - Pacific Fund A (USD)", ret.extractName());
+    public void testGettingThePrice() {
+        assertEquals("27,74$", extractor.extractPrice());
+    }
+
+    @Test
+    public void testGettingTheName() {
+        assertEquals("Fidelity Funds - Pacific Fund A (USD)",
+                extractor.extractName());
     }
 }
