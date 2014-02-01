bb5aa5309014fc3f12f699a59a3adf75afd06ab4
xorrr <derxor@gmail.com>
2014-02-01 23:05:54 +0100

    add the new growthrate functionality to the FundValuesExtractor integration test

--- src/test/java/org/xorrr/financegrabber/retrieval/TestFundValuesExtractorOnline.java
+++ src/test/java/org/xorrr/financegrabber/retrieval/TestFundValuesExtractorOnline.java
@@ -1,10 +1,11 @@
 package org.xorrr.financegrabber.retrieval;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.io.IOException;
 
 import org.jsoup.nodes.Document;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.xorrr.financegrabber.helpers.IntegrationTest;
@@ -12,20 +13,31 @@ import org.xorrr.financegrabber.helpers.IntegrationTest;
 @Category(IntegrationTest.class)
 public class TestFundValuesExtractorOnline {
 
-    @Test
-    public void testFidelityDocumentRetriever() throws IOException,
-            InvalidIsinException {
+    private FundValuesExtractor extractor; 
+
+    @Before
+    public void setUp() throws IOException, InvalidIsinException {
+        this.extractor = new FundValuesExtractor();
         FidelityFundDocumentAccessor da = new FidelityFundDocumentAccessor();
         Document doc = da.getDocumentForIsin("LU0049112450");
-        FundValuesExtractor extractor = new FundValuesExtractor();
+
         extractor.useDocument(doc);
-        assertEquals("Fidelity Funds - Pacific Fund A (USD)",
-                extractor.extractName());
     }
 
     @Test(expected = InvalidIsinException.class)
     public void testFidelityDocumentRetrieverWithWrongIsin()
             throws IOException, InvalidIsinException {
         new FidelityFundDocumentAccessor().getDocumentForIsin("foobarbaz");
+    }
+
+    @Test
+    public void testGettingTheDifferentValues() {
+        assertEquals("Extracting Name", "Fidelity Funds - Pacific Fund A (USD)",
+                extractor.extractName());
+        assertTrue("Extracting Price", extractor.extractPrice().length() > 2);
+        assertTrue("Extracting CurrentGrowth", extractor.extractCurrentYearGrowth().length() > 2);
+        assertTrue("Extracting OneYearGrowth", extractor.extractOneYearGrowth().length() > 2);
+        assertTrue("Extracting ThreeYearGrowth", extractor.extractThreeYearGrowth().length() > 2);
+        assertTrue("Extracting FiveYearGrowth", extractor.extractFiveYearGrowth().length() > 2);
     }
 }
