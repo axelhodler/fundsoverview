0dcf8516e77c6621ede51dc61caa7915541f2bd0
xorrr <derxor@gmail.com>
2014-02-02 01:56:21 +0100

    extend the fundproduct pojo with the growthrate fields

--- src/main/java/org/xorrr/financegrabber/model/FundProduct.java
+++ src/main/java/org/xorrr/financegrabber/model/FundProduct.java
@@ -5,6 +5,10 @@ public class FundProduct {
     private String isin;
     private String name;
     private String currentPrice;
+    private String currentGrowth;
+    private String oneYearGrowth;
+    private String threeYearGrowth;
+    private String fiveYearGrowth;
 
     public static class Builder {
         private String isin;
@@ -41,5 +45,37 @@ public class FundProduct {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getCurrentGrowth() {
+        return currentGrowth;
+    }
+
+    public void setCurrentGrowth(String currentGrowth) {
+        this.currentGrowth = currentGrowth;
+    }
+
+    public String getOneYearGrowth() {
+        return oneYearGrowth;
+    }
+
+    public void setOneYearGrowth(String oneYearGrowth) {
+        this.oneYearGrowth = oneYearGrowth;
+    }
+
+    public String getThreeYearGrowth() {
+        return threeYearGrowth;
+    }
+
+    public void setThreeYearGrowth(String threeYearGrowth) {
+        this.threeYearGrowth = threeYearGrowth;
+    }
+
+    public String getFiveYearGrowth() {
+        return fiveYearGrowth;
+    }
+
+    public void setFiveYearGrowth(String fiveYearGrowth) {
+        this.fiveYearGrowth = fiveYearGrowth;
     }
 }
