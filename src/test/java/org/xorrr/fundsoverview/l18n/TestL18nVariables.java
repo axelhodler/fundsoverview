5db208fd1d3308cb07a85cef63ba8d72fd940a82
xorrr <derxor@gmail.com>
2014-02-04 00:27:56 +0100

    test the german variables too

--- src/test/java/org/xorrr/fundsoverview/l18n/TestL18nVariables.java
+++ src/test/java/org/xorrr/fundsoverview/l18n/TestL18nVariables.java
@@ -2,16 +2,39 @@ package org.xorrr.fundsoverview.l18n;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestL18nVariables {
 
+    private L18nHelper helper;
+
+    @Before
+    public void setUp() {
+        helper = new L18nHelper();
+    }
+
     @Test
     public void doDefinedVariablesExistForEn() {
-        L18nHelper helper = new L18nHelper();
-        ResourceBundle messages = helper.getMessages();
+        Locale en = new Locale("en", "US");
+        ResourceBundle messages = helper.getMessages(en);
+
+        assertNotNull(messages.getString(L18nVariables.FUND));
+        assertNotNull(messages.getString(L18nVariables.PRICE));
+        assertNotNull(messages.getString(L18nVariables.CURRENT_YEAR));
+        assertNotNull(messages.getString(L18nVariables.ONE_YEAR));
+        assertNotNull(messages.getString(L18nVariables.THREE_YEARS));
+        assertNotNull(messages.getString(L18nVariables.FIVE_YEARS));
+        assertNotNull(messages.getString(L18nVariables.ADD_FUND));
+    }
+
+    @Test
+    public void doDefinedVariablesExistForDe() {
+        Locale ger = new Locale("de", "DE");
+        ResourceBundle messages = helper.getMessages(ger);
 
         assertNotNull(messages.getString(L18nVariables.FUND));
         assertNotNull(messages.getString(L18nVariables.PRICE));
