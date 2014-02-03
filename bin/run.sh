0edf006bf004689a364b70c39a81c2ae3ca3b3c1
xorrr <derxor@gmail.com>
2014-02-04 00:46:16 +0100

    add env vars LANG and COUNTRY to the scripts

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 export MONGODB_URI=mongodb://localhost:27017
+export LANG=en
+export COUNTRY=EN
 
 mvn jetty:run
--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 export MONGODB_URI=mongodb://localhost:12345
+export LANG=en
+export COUNTRY=EN
 
 mvn test
