5142031b2d4dc093f6c11cbd09a885b76a234a72
xorrr <derxor@gmail.com>
2014-02-04 00:17:00 +0100

    add l18n vars

--- src/main/java/org/xorrr/fundsoverview/l18n/L18nVariables.java
+++ src/main/java/org/xorrr/fundsoverview/l18n/L18nVariables.java
@@ -7,4 +7,5 @@ public class L18nVariables {
     public static final String ONE_YEAR = "oneYear";
     public static final String THREE_YEARS = "threeYears";
     public static final String FIVE_YEARS = "fiveYears";
+    public static final String ADD_FUND = "addFund";
 }
