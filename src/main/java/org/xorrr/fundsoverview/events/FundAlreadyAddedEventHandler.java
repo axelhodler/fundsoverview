665cfec672a11629f1197f4df1496af2e7fea757
xorrr <derxor@gmail.com>
2014-02-07 02:19:42 +0100

    use the new properties in the handlers

--- src/main/java/org/xorrr/fundsoverview/events/FundAlreadyAddedEventHandler.java
+++ src/main/java/org/xorrr/fundsoverview/events/FundAlreadyAddedEventHandler.java
@@ -1,13 +1,23 @@
 package org.xorrr.fundsoverview.events;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.xorrr.fundsoverview.l18n.L18nHelper;
+import org.xorrr.fundsoverview.l18n.L18nVariables;
+
 import com.vaadin.server.Page;
 import com.vaadin.ui.Notification;
 
-public class FundAlreadyAddedEventHandler implements EventHandler{
+public class FundAlreadyAddedEventHandler implements EventHandler {
 
     @Override
     public void handleEvent(EventType t) {
-        Notification notif = new Notification("Isin already added");
+        L18nHelper helper = new L18nHelper();
+        ResourceBundle bundle = helper.getMessages(new Locale(System
+                .getenv("LANG"), System.getenv("COUNTRY")));
+        Notification notif = new Notification(
+                bundle.getString(L18nVariables.FUND_ALREADY_ADDED));
         notif.show(Page.getCurrent());
     }
 
--- src/main/java/org/xorrr/fundsoverview/events/InvalidIsinEventHandler.java
+++ src/main/java/org/xorrr/fundsoverview/events/InvalidIsinEventHandler.java
@@ -1,5 +1,11 @@
 package org.xorrr.fundsoverview.events;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.xorrr.fundsoverview.l18n.L18nHelper;
+import org.xorrr.fundsoverview.l18n.L18nVariables;
+
 import com.vaadin.server.Page;
 import com.vaadin.ui.Notification;
 
@@ -7,7 +13,11 @@ public class InvalidIsinEventHandler implements EventHandler {
 
     @Override
     public void handleEvent(EventType t) {
-        Notification notif = new Notification("Isin is invalid");
+        L18nHelper helper = new L18nHelper();
+        ResourceBundle bundle = helper.getMessages(new Locale(System
+                .getenv("LANG"), System.getenv("COUNTRY")));
+        Notification notif = new Notification(
+                bundle.getString(L18nVariables.ISIN_INVALID));
         notif.show(Page.getCurrent());
     }
 
