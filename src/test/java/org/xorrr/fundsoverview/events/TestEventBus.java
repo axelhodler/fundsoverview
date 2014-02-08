01a5f5b677a5358f4ff87e2a5e71a69fc3b23839
xorrr <derxor@gmail.com>
2014-02-08 16:04:52 +0100

    create eventhandler to deal with wrong credentials

--- src/test/java/org/xorrr/fundsoverview/events/TestEventBus.java
+++ src/test/java/org/xorrr/fundsoverview/events/TestEventBus.java
@@ -12,15 +12,20 @@ public class TestEventBus {
     public void testEventBus() {
         EventHandler handler = mock(FundAlreadyAddedEventHandler.class);
         EventHandler secondHandler = mock(InvalidIsinEventHandler.class);
+        EventHandler wrongCredentials = mock(WrongCredentialsHandler.class);
         EventBus bus = EventBus.getInstance();
         bus.addHandler(EventType.FUND_ALREADY_ADDED, handler);
         bus.addHandler(EventType.INVALID_ISIN, secondHandler);
         bus.addHandler(EventType.INVALID_ISIN, handler);
+        bus.addHandler(EventType.WRONG_CREDENTIALS, wrongCredentials);
+
         bus.fireEvent(EventType.FUND_ALREADY_ADDED);
         bus.fireEvent(EventType.INVALID_ISIN);
+        bus.fireEvent(EventType.WRONG_CREDENTIALS);
 
         verify(handler, times(1)).handleEvent(EventType.FUND_ALREADY_ADDED);
         verify(handler, times(1)).handleEvent(EventType.INVALID_ISIN);
         verify(secondHandler, times(1)).handleEvent(EventType.INVALID_ISIN);
+        verify(wrongCredentials, times(1)).handleEvent(EventType.WRONG_CREDENTIALS);
     }
 }
