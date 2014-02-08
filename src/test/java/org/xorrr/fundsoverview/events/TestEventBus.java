4bb13aaff1fc5cd25a4a12f2e8ffb14dd088dd6a
xorrr <derxor@gmail.com>
2014-02-08 16:58:53 +0100

    refactor the EventBus tests

--- src/test/java/org/xorrr/fundsoverview/events/TestEventBus.java
+++ src/test/java/org/xorrr/fundsoverview/events/TestEventBus.java
@@ -4,28 +4,44 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestEventBus {
+    private EventBus bus;
+
+    @Before
+    public void setUp() {
+        bus = EventBus.getInstance();
+    }
 
     @Test
-    public void testEventBus() {
-        EventHandler handler = mock(FundAlreadyAddedEventHandler.class);
-        EventHandler secondHandler = mock(InvalidIsinEventHandler.class);
-        EventHandler wrongCredentials = mock(WrongCredentialsHandler.class);
-        EventBus bus = EventBus.getInstance();
-        bus.addHandler(EventType.FUND_ALREADY_ADDED, handler);
-        bus.addHandler(EventType.INVALID_ISIN, secondHandler);
-        bus.addHandler(EventType.INVALID_ISIN, handler);
-        bus.addHandler(EventType.WRONG_CREDENTIALS, wrongCredentials);
+    public void fundAlreadyEventIsHandled() {
+        EventHandler fundAlreadyAddedHandler = mock(FundAlreadyAddedEventHandler.class);
+        bus.addHandler(EventType.FUND_ALREADY_ADDED, fundAlreadyAddedHandler);
 
         bus.fireEvent(EventType.FUND_ALREADY_ADDED);
+
+        verify(fundAlreadyAddedHandler, times(1)).handleEvent(EventType.FUND_ALREADY_ADDED);
+    }
+
+    @Test
+    public void invalidIsinIsHandled() {
+        EventHandler invalidIsinHandler = mock(InvalidIsinEventHandler.class);
+        bus.addHandler(EventType.INVALID_ISIN, invalidIsinHandler);
+
         bus.fireEvent(EventType.INVALID_ISIN);
+
+        verify(invalidIsinHandler, times(1)).handleEvent(EventType.INVALID_ISIN);
+    }
+
+    @Test
+    public void wrongCredentialsAreHandled() {
+        EventHandler wrongCredentials = mock(WrongCredentialsHandler.class);
+        bus.addHandler(EventType.WRONG_CREDENTIALS, wrongCredentials);
+
         bus.fireEvent(EventType.WRONG_CREDENTIALS);
 
-        verify(handler, times(1)).handleEvent(EventType.FUND_ALREADY_ADDED);
-        verify(handler, times(1)).handleEvent(EventType.INVALID_ISIN);
-        verify(secondHandler, times(1)).handleEvent(EventType.INVALID_ISIN);
         verify(wrongCredentials, times(1)).handleEvent(EventType.WRONG_CREDENTIALS);
     }
 }
