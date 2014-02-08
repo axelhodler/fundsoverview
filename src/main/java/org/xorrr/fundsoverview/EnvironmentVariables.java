07daf4c31d6116c0498ce9456101030de91be677
xorrr <derxor@gmail.com>
2014-02-08 01:10:05 +0100

    add environmentvariables as constants to a class

---
+++ src/main/java/org/xorrr/fundsoverview/EnvironmentVariables.java
@@ -0,0 +1,9 @@
+package org.xorrr.fundsoverview;
+
+public abstract class EnvironmentVariables {
+    public static final String MONGODB_URI = "MONGODB_URI";
+    public static final String LANG = "LANG";
+    public static final String COUNTRY = "COUNTRY";
+    public static final String USER= "USER";
+    public static final String PASS = "PASS";
+}
