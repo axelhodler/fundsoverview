0dc52cc95cf4da1f02f37f5c45d4ebf80a20b443
xorrr <derxor@gmail.com>
2014-02-19 18:55:29 +0100

    add tests for component existence and their translation

--- src/main/java/org/xorrr/fundsoverview/layouts/AddFundLayout.java
+++ src/main/java/org/xorrr/fundsoverview/layouts/AddFundLayout.java
@@ -1,19 +1,26 @@
 package org.xorrr.fundsoverview.layouts;
 
 import org.xorrr.fundsoverview.l18n.Localization;
+import org.xorrr.fundsoverview.l18n.LocalizationStrings;
 import org.xorrr.fundsoverview.view.DashboardView;
 
+import com.vaadin.ui.CustomLayout;
+import com.vaadin.ui.TextField;
 
-public class AddFundLayout {
+public class AddFundLayout extends CustomLayout {
+
+    private static final long serialVersionUID = -9041098835268681127L;
+    private TextField fundField;
 
     public void init() {
         Localization.getMessages();
+
+        fundField = new TextField(LocalizationStrings.FUND);
+        addComponent(fundField, AddFundLayoutLocations.FUND_FIELD);
     }
 
     public void setView(DashboardView view) {
-        // TODO Auto-generated method stub
-        
+
     }
 
-    
 }
--- src/test/java/org/xorrr/fundsoverview/layouts/TestAddFundLayout.java
+++ src/test/java/org/xorrr/fundsoverview/layouts/TestAddFundLayout.java
@@ -1,5 +1,8 @@
 package org.xorrr.fundsoverview.layouts;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import java.util.ResourceBundle;
 
 import org.junit.Before;
@@ -10,7 +13,10 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.xorrr.fundsoverview.l18n.Localization;
+import org.xorrr.fundsoverview.l18n.LocalizationStrings;
 import org.xorrr.fundsoverview.view.DashboardViewImpl;
+
+import com.vaadin.ui.Component;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({ Localization.class })
@@ -23,6 +29,17 @@ public class TestAddFundLayout {
 
     private AddFundLayout layout;
 
+    private void checkComponentExistence(String location) {
+        Component component = layout.getComponent(location);
+        assertNotNull(component);
+    }
+
+    private void checkComponentTranslation(String location,
+            String expectedTranslation) {
+        Component component = layout.getComponent(location);
+        assertEquals(expectedTranslation, component.getCaption());
+    }
+
     @Before
     public void setUp() {
         PowerMockito.mockStatic(Localization.class);
@@ -34,8 +51,19 @@ public class TestAddFundLayout {
     }
 
     @Test
-    public void localizationIsSet() {
+    public void localizationIsAccessed() {
         PowerMockito.verifyStatic();
         Localization.getMessages();
+    }
+
+    @Test
+    public void fundFieldCreated() {
+        checkComponentExistence(AddFundLayoutLocations.FUND_FIELD);
+    }
+
+    @Test
+    public void fundFieldCaptionTranslated() {
+        checkComponentTranslation(AddFundLayoutLocations.FUND_FIELD,
+                LocalizationStrings.FUND);
     }
 }
