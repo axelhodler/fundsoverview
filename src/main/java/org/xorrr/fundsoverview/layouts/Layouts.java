07d44bd4b47eaa6bb5225aa7dba3145debd2699b
xorrr <derxor@gmail.com>
2014-02-19 23:27:12 +0100

    add fundaddform to the layouts constants

--- src/main/java/org/xorrr/fundsoverview/layouts/Layouts.java
+++ src/main/java/org/xorrr/fundsoverview/layouts/Layouts.java
@@ -3,4 +3,5 @@ package org.xorrr.fundsoverview.layouts;
 public abstract class Layouts {
     public static final String DASHBOARD = "dashboard";
     public static final String LOGIN = "login";
+    public static final String FUND_ADD = "fundaddform";
 }
