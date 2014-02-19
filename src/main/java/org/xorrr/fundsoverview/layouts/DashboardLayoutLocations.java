ff0df06bfaa158e36bf9b85b89bee66fb2ef5f2c
xorrr <derxor@gmail.com>
2014-02-19 22:08:54 +0100

    create constants for the locations on the dashboard template

---
+++ src/main/java/org/xorrr/fundsoverview/layouts/DashboardLayoutLocations.java
@@ -0,0 +1,8 @@
+package org.xorrr.fundsoverview.layouts;
+
+public abstract class DashboardLayoutLocations {
+    public static final String FUNDSTABLE = "fundstable";
+    public static final String LOGIN = "login";
+    public static final String FUND_ID= "fundId";
+    public static final String ADD_FUND = "addFund";
+}
