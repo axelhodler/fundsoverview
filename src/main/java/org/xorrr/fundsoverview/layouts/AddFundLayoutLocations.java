4f666fba5e6536c4746ff87b08a49ac074550be3
xorrr <derxor@gmail.com>
2014-02-19 18:55:09 +0100

    add class with constants for used locations in the addFundLayout template to come

---
+++ src/main/java/org/xorrr/fundsoverview/layouts/AddFundLayoutLocations.java
@@ -0,0 +1,6 @@
+package org.xorrr.fundsoverview.layouts;
+
+public abstract class AddFundLayoutLocations {
+    public static final String FUND_FIELD = "fundField";
+    public static final String ADD_FUND = "addFund";
+}
