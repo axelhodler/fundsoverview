6365aff26c4ad31d8c0e917d98039a9bf0e60fb9
xorrr <derxor@gmail.com>
2014-02-20 02:45:53 +0100

    improve layout naming

--- src/main/java/org/xorrr/fundsoverview/layouts/Layouts.java
+++ src/main/java/org/xorrr/fundsoverview/layouts/Layouts.java
@@ -3,5 +3,5 @@ package org.xorrr.fundsoverview.layouts;
 public abstract class Layouts {
     public static final String DASHBOARD = "dashboard";
     public static final String LOGIN = "login";
-    public static final String FUND_ADD = "fundaddform";
+    public static final String FUND_ADD = "addfundform";
 }
